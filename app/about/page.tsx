993d6feea084aee020e8cf95f39e2efcdc4898c2
Yusuf Iysah  | UX Engineer <88023259+Iysah@users.noreply.github.com>
2025-01-02 07:24:49 +0100

    UI: update about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -46,7 +46,7 @@ export default function About() {
                   src="https://images.pexels.com/photos/8553867/pexels-photo-8553867.jpeg?auto=compress&cs=tinysrgb&w=800"
                   alt="Yawa App Team"
                   fill
-                  className="object-cover rounded-lg"
+                  className="object-contain rounded-lg"
                 />
               </div>
               <div>
